561736d40cb88b29a800c3dd2ec73e73991966ac
Rishabh <mayankpandey7773@gmail.com>
2024-02-16 19:46:51 +0530

    rrraga

---
+++ pattern2.py
@@ -0,0 +1,20 @@
+from turtle import*
+speed(0)
+
+
+def polygon(side):
+    fillcolor(color)
+    begin_fill()
+    for _ in range(side):
+        fd(69) 
+        lt(360/side)
+    end_fill    
+
+
+
+for i in range(6):
+         
+
+
+
+        
