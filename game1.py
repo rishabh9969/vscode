b7641509a32d5423d8c8761f176adef0a38fd3ec
Rishabh <mayankpandey7773@gmail.com>
2024-02-29 19:42:41 +0530

    numpy

---
+++ game1.py
@@ -0,0 +1,14 @@
+import pgzrun
+
+WIDTH=800
+HEIGHT=500
+
+def draw():
+    screen.draw.text(
+        "hello,pygame zero!",
+        center=(WIDTH/2, HEIGHT/2),
+        fontsize=32,
+        color="white"
+
+ )
+    def update()
